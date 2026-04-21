891a042c713b10df2122b123cb9f56bb23c93cd8
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-04-20 22:46:27 -0300

    fix: add 10s for timout

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,7 +3,7 @@ import { CountryFieldsList } from "./fields-list"
 
 export const api = axios.create({
   baseURL: "https://restcountries.com/v3.1",
-  timeout: 1000,
+  timeout: 10000,
   params: {
     fields: Object.values(CountryFieldsList).join(","),
   },
